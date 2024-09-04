fa994b1ce88df142b07ceb8edf35cfcbe192d0f9
asmaa-aljazzar <126964381+asmaa-aljazzar@users.noreply.github.com>
2024-09-04 09:19:43 +0300

    Update ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,8 @@
+void    *ft_memset(void *b, int c, size_t len)
+{
+     unsigned char *temp; 
+    /* make this temporary variable equals to void *b converted to unsigned char */
+    /* loop on the temporary variable while we didn't reach len */
+        /* in that loop, set the current byte equal to c converted to unsigned char */
+    /* return void *b */
+}
