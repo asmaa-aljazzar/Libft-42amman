7cce186e2415040677af466020825fe35ddc12ce
asmaa-aljazzar <126964381+asmaa-aljazzar@users.noreply.github.com>
2024-09-04 09:23:32 +0300

    Update ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,7 @@
+void    ft_bzero(void *s, size_t n)
+{
+    /* declare a temporary pointer */
+    /* make the temporary pointer equal to *s converted to a char * */
+    /* loop on the temporary pointer while we didn't reach n characters */
+        /* in that loop, set the current byte equal to 0 */
+}
